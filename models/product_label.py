07b4cf0b2420e2eee24eafb25b1fe866031e0e5e
Sean Thawe <sean.thawe@outlook.com>
2025-11-02 03:34:17 +0200

    refactor(wizard): Comment out start row/col and skipped pages fields

--- models/product_label.py
+++ models/product_label.py
@@ -53,9 +53,9 @@ class ProductLabelWizard(models.TransientModel):
             self.rows = 10
             self.cols = 3
 
-    skipped_pages = fields.Integer(string="Skip Full Pages", default=0)
-    start_row = fields.Integer(string="Start Row", default=1)
-    start_col = fields.Integer(string="Start Column", default=1)
+    # skipped_pages = fields.Integer(string="Skip Full Pages", default=0)
+    # start_row = fields.Integer(string="Start Row", default=1)
+    # start_col = fields.Integer(string="Start Column", default=1)
 
     @api.model
     def default_get(self, fields_list):
@@ -123,12 +123,12 @@ class ProductLabelWizard(models.TransientModel):
         self.ensure_one()
         if not self.product_ids:
             raise UserError(_("You must select at least one product."))
-        if self.start_row < 1 or self.start_row > self.rows:
-            raise UserError(_("Start Row must be between 1 and %d.") % self.rows)
-        if self.start_col < 1 or self.start_col > self.cols:
-            raise UserError(_("Start Column must be between 1 and %d.") % self.cols)
-        if self.skipped_pages < 0:
-            raise UserError(_("Skipped Pages must be a positive number."))
+        # if self.start_row < 1 or self.start_row > self.rows:
+        #     raise UserError(_("Start Row must be between 1 and %d.") % self.rows)
+        # if self.start_col < 1 or self.start_col > self.cols:
+        #     raise UserError(_("Start Column must be between 1 and %d.") % self.cols)
+        # if self.skipped_pages < 0:
+        #     raise UserError(_("Skipped Pages must be a positive number."))
 
     def _calculate_font_size(self, base_font_size):
         """Calculate a scaled font size based on rows and columns."""
@@ -253,8 +253,8 @@ class ProductLabelWizard(models.TransientModel):
         )
         
         # Add offsets for skipped cells
-        offset = (self.start_row - 1) * self.cols + (self.start_col - 1)
-        all_labels = ([{}] * offset) + all_labels
+        # offset = (self.start_row - 1) * self.cols + (self.start_col - 1)
+        # all_labels = ([{}] * offset) + all_labels
 
         # Calculate page numbers
         if self.rows <= 0 or self.cols <= 0:
